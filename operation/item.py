57983a4d8a1b620d6d0726be7bc639d7bd9b4858
atongmuzi <jazx505@163.com>
2021-09-27 19:37:28 +0800

    add new cases

--- operation/item.py
+++ operation/item.py
@@ -22,6 +22,16 @@ def item_next_true(item_id, first_random):
     return res_deal(res)
 
 
+def item_status(item_id, box_no):
+    """
+    实时查看线盒的状态
+    :return: 自定义返回字段 result
+    """
+    result = ResultBase()
+    res = item.item_status(item_id=item_id, box_no=box_no, headers=headers)
+    return res_deal(res)
+
+
 def item_pick_sku(item_id, sku_no):
     """
       用户第一次进来随机选中的一个线盒
--- testcases/api_test/test_07_item.py
+++ testcases/api_test/test_07_item.py
@@ -1,6 +1,6 @@
 import pytest
 import allure
-from operation.item import item_next_true, item_pick_sku
+from operation.item import item_next_true, item_pick_sku,item_status
 from common.logger import logger
 import random
 
@@ -26,29 +26,47 @@ def step_3():
 class TestTradePreItem:
     sku_no = ""
     box_no = ""
-    """下一个盲盒详情"""
+
     @allure.story("下一个盲盒详情")
-    @pytest.fixture()
     def test_item_next(self):
+        """下一个盲盒详情"""
         step_1()
         item_id = 378
         first_random = True
         logger.info("*************** 开始执行用例 *************** /n")
         result = item_next_true(item_id, first_random)
         itemSelect.box_no = result.data["mbox"]["box_no"]
-        index = random.randint(0, 5)
-        itemSelect.sku_no = result.data["mbox"]["cell_list"][index]["sku_no"]
-        logger.info(itemSelect.sku_no)
+        itemSelect.size = result.data["mbox"]["size"]
+        logger.info("box_no==>>{}".format(itemSelect.box_no))
         logger.info("*************** 结束执行用例 *************** /n")
-        if result.success:
+
+    @allure.story("实时查看盒子状态--是否被占用")
+    @pytest.fixture()
+    def test_item_status(self):
+        """实时查看盒子状态--是否被占用"""
+        step_2()
+        item_id = 378
+        box_no = itemSelect.box_no
+        logger.info("*************** 开始执行用例 ***************")
+        result = item_status(item_id, box_no)
+        index = itemSelect.size
+        itemSelect.status = False
+        for i in range(index-1):
+            if result.data[i]["status"] == 0:
+                itemSelect.sku_no = result.data[i]["sku_no"]
+                itemSelect.Status = True
+                break
+        logger.info("sku_no==>>{}".format(itemSelect.sku_no))
+        logger.info("*************** 结束执行用例 *************** /n")
+        if itemSelect.Status:
             return True
         else:
             return False
 
-    """选中其中一个盒子"""
     @allure.story("选中一个盒子")
-    def test_item_pick(self, test_item_next):
-        next_result = test_item_next
+    def test_item_pick(self, test_item_status):
+        """选中其中一个盒子"""
+        next_result = test_item_status
         step_3()
         if next_result:
             item_id = 378
