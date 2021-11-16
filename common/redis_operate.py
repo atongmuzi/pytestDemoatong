e159f7bc08f4ed29916c86569a63c40801d79a39
atongmuzi <jazx505@163.com>
2021-11-16 10:58:56 +0800

    add

--- common/redis_operate.py
+++ common/redis_operate.py
@@ -2,6 +2,8 @@ import redis
 from common.logger import logger
 import os
 from common.read_data import data
+import re
+import sys
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 data_file_path = os.path.join(BASE_PATH, "config", "setting.ini")
@@ -36,11 +38,22 @@ class RedisOperate:
         r = redis.Redis(connection_pool=pool)
         logger.info("redis缓存链接为：===>{}".format(r))
         token_key = "pookie:token:user_%s" % user_id
+        logger.info("token_key为===》{}".format(token_key))
         value = r.get(token_key)
-        logger.info("token是===>{}".format(value))
+        logger.info("value===》{}".format(value))
+        value_new = value.decode("utf-16")
+        logger.info("解码后的value_new为：===》{}，type是{}".format(value_new, type(value_new)))
+        result = re.findall(r"\"token\":\"(.*)\"", value_new)
+        logger.info("匹配结果是===》{}".format(result))
+        s_str = "中文"
+        b_str = bytes(s_str, encoding="utf-16")
+        logger.info(b_str)
+        str_new = str(b_str, encoding="utf-16")
+        logger.info(str_new)
+        logger.info(sys.getdefaultencoding())
 
 
 rs = RedisOperate()
 
 if __name__ == '__main__':
-    rs.test_token_get(1346)
+    rs.test_token_get(1478)
