e9de60dab60faea42a4e828a5a2b4bb29ce8b782
atongmuzi <jazx505@163.com>
2021-11-18 11:43:38 +0800

    add conf.py

--- common/authorization_get.py
+++ common/authorization_get.py
@@ -3,20 +3,22 @@ import base64
 from common.logger import logger
 
 
-class Authorization:
+def test_authorization_get(user_id, channel_type):
+    token = rs.test_token_get(user_id)
+    logger.info("token===》{}".format(token))
+    token_new = str(user_id) + ":" + token + ":" + str(channel_type)
+    token_new_encode = token_new.encode("utf-8")
+    logger.info("token_new_encode===》{}".format(token_new_encode))
+    authorization = base64.b64encode(token_new_encode)
+    authorization_new = authorization.decode("utf-8")
+    logger.info("authorization为===》{}".format(authorization_new))
 
-    def test_authorization_get(self, user_id, channel_type):
-        token = rs.test_token_get(user_id)
-        logger.info("token===》{}".format(token))
-        token_new = str(user_id) + ":" + token + ":" + str(channel_type)
-        token_new_encode = token_new.encode("utf-8")
-        logger.info("token_new_encode===》{}".format(token_new_encode))
-        authorization = base64.b64encode(token_new_encode)
-        authorization_new = authorization.decode("utf-8")
-        logger.info("authorization为===》{}".format(authorization_new))
+
+class Authorization:
+    pass
 
 
 au = Authorization()
 
 if __name__ == '__main__':
-    au.test_authorization_get(1528, 2)
+    test_authorization_get(1528, 2)
