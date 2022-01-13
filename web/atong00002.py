30d74ccd3c72a7f733ca81e0dc7459cf6f93d2ec
atongmuzi <jazx505@163.com>
2022-01-13 16:07:39 +0800

    modify userinfo.yml

--- web/atong00002.py
+++ web/atong00002.py
@@ -4,6 +4,9 @@ from flask import Flask, request
 from common.mysql_operate import db
 
 app = Flask(__name__)
+app.config["SWAGGER"] = {
+    "title": "pookie API"
+}
 Swagger(app)
 
 
@@ -12,21 +15,39 @@ Swagger(app)
 def user_info():
     """根据手机号查询用户相关信息"""
     global user_id, nickname
-    phone = request.args.get('phone', 1)
-    sql = "select id as user_id,nickname from user where phone ='%s' " % phone
-    data = db.select_db(sql)
-    size = int(request.args.get('size', 1))
-    logger.info("data===>{}".format(data))
-    for i in range(len(data)):
-        logger.info(data[i]["user_id"])
-        user_id = data[i]["user_id"]
-        nickname = data[i]["nickname"]
-    json_data = {
-        "用户ID": user_id,
-        "用户昵称": nickname,
-        "长度": size
-    }
-    return json_data
+    phone = request.args.get("phone")
+    nickname = request.args.get("nickname")
+    if phone is None and nickname is None:
+        return "麻烦输入手机号或者昵称"
+    elif phone is not None:
+        sql = "select id as user_id,nickname from user where phone ='%s' " % phone
+        data = db.select_db(sql)
+        if data:
+            for i in range(len(data)):
+                logger.info(data[i]["user_id"])
+                user_id = data[i]["user_id"]
+                nickname = data[i]["nickname"]
+            json_data = {
+                "手机号": phone,
+                "用户ID": user_id,
+                "用户昵称": nickname,
+            }
+            return json_data
+
+        else:
+            return "无该手机号用户相关信息，请重新输入手机号或者昵称"
+    else:
+        sql = "select id as user_id,nickname from user where nickname ='%s' " % nickname
+        data = db.select_db(sql)
+        logger.info("data===>{}".format(data))
+        for i in range(len(data)):
+            user_id = data[i]["user_id"]
+            nickname = data[i]["nickname"]
+        json_data = {
+            "用户ID": user_id,
+            "用户昵称": nickname
+        }
+        return json_data
 
 
 app.run(debug=True)
