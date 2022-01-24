7010ac53c20c3c13fd627b425dbef63227ee7ecd
atongmuzi <jazx505@163.com>
2022-01-24 15:45:22 +0800

    hahaha

--- testcases/admin_test/test_refund_00001.py
+++ testcases/admin_test/test_refund_00001.py
@@ -6,7 +6,7 @@ from common.mysql_operate import db
 class TestAdminRefund:
 
     def test_refund(self):
-        sql = "select order_no from user_order where user_id in('1528') and" \
+        sql = "select order_no from user_order where user_id =1528 and" \
               " status not in(0,30,31,300) and real_price >= 0.1 limit 3 "
         data = db.select_db(sql)
         logger.info("data===>{}".format(data))
--- web/atong00002.py
+++ web/atong00002.py
@@ -5,7 +5,8 @@ from common.mysql_operate import db
 
 app = Flask(__name__)
 app.config["SWAGGER"] = {
-    "title": "pookie API"
+    "title": "pookie API",
+    "version": "1.0.0"
 }
 Swagger(app)
 
@@ -14,11 +15,26 @@ Swagger(app)
 @swag_from('userinfo.yml')
 def user_info():
     """根据手机号查询用户相关信息"""
-    global user_id, nickname
+    global user_id, nickname, phone
     phone = request.args.get("phone")
     nickname = request.args.get("nickname")
-    if phone is None and nickname is None:
-        return "麻烦输入手机号或者昵称"
+    if phone and nickname:
+        sql = "select id as user_id,nickname from user where nickname ='%s' and phone = '%s' " % (nickname, phone)
+        data = db.select_db(sql)
+        if data:
+            for i in range(len(data)):
+                logger.info(data[i]["user_id"])
+                user_id = data[i]["user_id"]
+                nickname = data[i]["nickname"]
+            json_data = {
+                "手机号": phone,
+                "用户ID": user_id,
+                "用户昵称": nickname,
+            }
+            return json_data
+
+        else:
+            return "请检查输入的手机号或者昵称有无问题"
     elif phone is not None:
         sql = "select id as user_id,nickname from user where phone ='%s' " % phone
         data = db.select_db(sql)
@@ -36,7 +52,7 @@ def user_info():
 
         else:
             return "无该手机号用户相关信息，请重新输入手机号或者昵称"
-    else:
+    elif nickname is not None:
         sql = "select id as user_id,nickname from user where nickname ='%s' " % nickname
         data = db.select_db(sql)
         logger.info("data===>{}".format(data))
@@ -48,6 +64,8 @@ def user_info():
             "用户昵称": nickname
         }
         return json_data
+    else:
+        return "麻烦输入手机号或者昵称"
 
 
 app.run(debug=True)
