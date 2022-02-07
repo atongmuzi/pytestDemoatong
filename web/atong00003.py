24db05e631a7102246f87bda34b9b26bd33210ea
atongmuzi <jazx505@163.com>
2022-02-07 14:22:11 +0800

    hello

---
+++ web/atong00003.py
@@ -0,0 +1,20 @@
+# from common.logger import logger
+# from flasgger import Swagger, swag_from
+# from flask import Flask, request
+# from common.mysql_operate import db
+#
+# app = Flask(__name__)
+# app.config["SWAGGER"] = {
+#     "title": "fragment",
+#     "version": "0.0.1"
+# }
+# Swagger(app)
+
+
+# @app.route('/api/fragment/', methods=['GET'])
+# @swag_from('/xml/fragment_info.xml')
+# def user_fragment_info():
+#     """
+#     给用户添加增加各类型碎片
+#     """
+
---
+++ web/test.py
@@ -0,0 +1,6 @@
+a = 10
+
+
+def test():
+
+    print(a)
