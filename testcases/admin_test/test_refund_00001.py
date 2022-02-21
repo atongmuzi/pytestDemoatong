9bf908b1fbd574a4a4dffe45a845fd2d8177f17a
atongmuzi <jazx505@163.com>
2022-02-21 15:01:55 +0800

    atong commit

--- testcases/admin_test/test_refund_00001.py
+++ testcases/admin_test/test_refund_00001.py
@@ -6,8 +6,8 @@ from common.mysql_operate import db
 class TestAdminRefund:
 
     def test_refund(self):
-        sql = "select order_no from user_order where user_id =1855 and" \
-              " status not in(0,30,31,300) and real_price >= 0.1 limit 3 "
+        sql = "select order_no from user_order where user_id =1528 and" \
+              " status not in(0,30,31,300) and real_price >= 0.1 limit 5 "
         data = db.select_db(sql)
         logger.info("data===>{}".format(data))
         for i in range(len(data)):
