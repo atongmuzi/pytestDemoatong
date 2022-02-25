eda2dcd8a26370be59513ff92add9b42864bc24d
atongmuzi <jazx505@163.com>
2022-02-25 15:42:29 +0800

    123

--- testcases/init_test/test_init_phone.py
+++ testcases/init_test/test_init_phone.py
@@ -40,16 +40,22 @@ class TestInitPhone:
         phone_new = userID + rm_result
         logger.info("随机数===>{}".format(rm_result))
         logger.info("随机更新的手机号===>{}".format(phone_new))
-        sql_up_user = "update user set phone = '%s' , phone_encrypt = '%s'  where id ='%s'"\
+        sql_up_user = "update user set phone = '%s' , phone_encrypt = '%s'  where id ='%s'" \
                       % (phone_new, phone_new, userID)
-        sql_up_wx = "update user_wechat_channel set open_id ='%s',union_id = '%s' " \
-                    "where user_id ='%s'" % (phone_new, phone_new, userID)
+        sql_sl_wx = "select id from user_wechat_channel where user_id = '%s' " % userID
         logger.info("----------开始更新user表----------")
         db.execute_db(sql_up_user)
         logger.info("----------执行更新user表成功----------")
-        logger.info("----------开始更新user_wechat_channel表----------")
-        db.execute_db(sql_up_wx)
-        logger.info("----------执行更新user_wechat_channel表成功----------")
+        data = db.select_db(sql_sl_wx)
+        for i in range(len(data)):
+            wechat_id = data[i]["id"]
+            logger.info("----------开始更新user_wechat_channel表----------")
+            rm_result = rm.generate_random_str(10)
+            phone_new = userID + rm_result
+            sql_up_wx = "update user_wechat_channel set open_id ='%s',union_id = '%s' " \
+                        "where id ='%s'" % (phone_new, phone_new, wechat_id)
+            db.execute_db(sql_up_wx)
+            logger.info("----------执行更新user_wechat_channel表成功----------")
         logger.info("----------执行清除缓存的操作----------")
         rs.test_redis(userID)
         logger.info("----------执行清除缓存操作成功----------")
